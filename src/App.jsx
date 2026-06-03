64e97086576072bc069f7fada7282f23df6a641e
Rémy <remy.pavy@soeti.fr>
2026-06-03 09:47:01 +0200

    Fix sync bidirectionnelle iPad/PC - push + pull au démarrage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import ProjectsList from './screens/ProjectsList'
 import ProjectDetail from './screens/ProjectDetail'
 import { syncAll } from './services/syncEngine'
